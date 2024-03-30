4f38b9ac38b1917434fc0b99c8d0f8f11dcb10a9
parthmodi444 <88581955+parthmodi444@users.noreply.github.com>
2024-03-30 23:24:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import productRoutes from "./routes/productRoutes.js"
 import connectDB from "./config/db.js"
 import path from "path"
 import cors from "cors"
-import {fileURLToPath} from "url
+import {fileURLToPath} from "url"
 
 const __filename=fileURLToPath(import.meta.url)
 const __dirname=path.dirname(__filename)
