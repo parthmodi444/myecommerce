eb41762efd99ca58a19b2f96cfc7ddd617698022
parthmodi444 <88581955+parthmodi444@users.noreply.github.com>
2024-03-30 22:33:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ import productRoutes from "./routes/productRoutes.js"
 import connectDB from "./config/db.js"
 import path from "path"
 import cors from "cors"
+import {fileURLToPath} from "url
 
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
 const app=express()
 
 const PORT=process.env.PORT || 8080
